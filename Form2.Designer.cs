64ca2c4902864a97db7f065a1e5e115cf230957a
Askarbek09 <askarbek.mamatair@gmail.com>
2023-03-28 15:48:27 +0200

    init commit

---
+++ Form2.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace BigBanTheory_Project4
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form2
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel5;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel4;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel3;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel2;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox nameBox;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox addressBox;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox numBox;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox lastBox;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox middleBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private Bunifu.Framework.UI.BunifuThinButton2 changeBtn;
+        private Bunifu.Framework.UI.BunifuThinButton2 backBtn;
+        private Bunifu.Framework.UI.BunifuThinButton2 saveBtn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton female;
+        private System.Windows.Forms.RadioButton male;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label path;
+    }
+}
